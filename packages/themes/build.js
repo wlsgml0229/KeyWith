c6b28311d90fe90ccdbea976480686ab0cc1e81a
wlsgml0229 <wlsgml0229@naver.com>
2024-07-24 22:03:09 +0900

    🔨 빌드파일 추가, 스크립트 수정

--- packages/themes/build.js
+++ packages/themes/build.js
@@ -1,22 +1,41 @@
-import esbuild from 'esbuild'
+import esbuild from "esbuild";
+import pkg from "./package.json" assert { type: "json" };
 
-esbuild.build({
-    entryPoints: ['src/index.js'],
-    bundle: true,
-    minify: true,
-    sourcemap: true,
-    outdir: 'dist',
-    format: 'esm'
-})
+const dev = process.argv.includes("--dev");
+const minify = !dev;
+const watch = process.argv.includes("--watch");
 
-esbuild.build({
-    entryPoints: ['src/index.js'],
-    bundle: true,
-    minify: true,
-    sourcemap: true,
-    outdir: 'dist',
-    format: 'cjs',
-    outExtension: {
-        ".js": ".cjs"
-    }
-})
+const external = Object.keys({
+  ...pkg.dependencies,
+  ...pkg.peerDependencies,
+});
+
+const baseConfig = {
+  entryPoints: ["src/index.js"],
+  bundle: true,
+  minify,
+  sourcemap: true,
+  outdir: "dist",
+  target: "es2019",
+  watch,
+  external,
+};
+
+Promise.all([
+  esbuild.build({
+    ...baseConfig,
+    format: "esm",
+  }),
+  esbuild
+    .build({
+      ...baseConfig,
+      format: "cjs",
+      outExtension: {
+        ".js": ".cjs",
+      },
+    })
+    .catch(() => {
+      console.log("....error");
+      process.exit(1);
+    }),
+]);
