3837b88296f8c7ef0f50c22048aefe59b91bb5da
wlsgml0229 <wlsgml0229@naver.com>
2024-07-28 16:06:16 +0900

    ✨ 색상팔레트 생성중

--- packages/themes/src/index.ts
+++ packages/themes/src/index.ts
@@ -1,12 +1,1 @@
-function sum(a: number, b: number) {
-  return a + b;
-}
-//commonJS기반
-// modules.exports = {
-//     sum
-// }
-
-//esmodule기반 ㅞ
-export default {
-  sum,
-};
+export * as vars from "./variables";
---
+++ packages/themes/src/variables/colors/static/light.ts
@@ -0,0 +1,29 @@
+export const green = {
+  1: "E6FAEF",
+  2: "91F7D3",
+  3: "00BC78",
+  4: "00B372",
+};
+
+export const gray = {
+  1: "#FFFFFF",
+  2: "#F7F8F9",
+  3: "#F1F2F4",
+  4: "#EAEBEE",
+  5: "#C6C8CE",
+  6: "#B4B8C2",
+  7: "#676F7B",
+  8: "#565D69",
+  9: "#484F5A",
+  10: "#0E0E0E",
+};
+
+export const blue = {
+  1: "#7580EB",
+  2: "#ECEDF9",
+};
+
+export const red = {
+  1: "#FB4E4E",
+  2: "#FFEBEE",
+};
